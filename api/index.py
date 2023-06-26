ff899b438f4b566c138f683938aa4ba1bf670969
willian <mynamewra@gmail.com>
2023-06-26 02:34:49 -0300

    Unfinished apis

--- api/index.py
+++ api/index.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_restful import reqparse, abort, Api, Resource
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
@@ -8,8 +8,6 @@ db = SQLAlchemy()
 app = Flask(__name__)
 api = Api(app)
 
-
-# api.add_resource(Bingo, '/todos/<todo_id>')
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
 db.init_app(app)
@@ -35,11 +33,30 @@ class CardModel(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     drawn_numbers = db.Column(db.String, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     bingo_id = db.Column(db.Integer, db.ForeignKey('bingo.id'), nullable=False)
-
-    def __init__(self, drawn_numbers):
+    number_card = db.Column(db.Integer, nullable=False)
+    
+    def __init__(self, drawn_numbers, bingo_id, user_id, number_card):
         self.drawn_numbers = drawn_numbers
+        self.bingo_id = bingo_id
+        self.user_id = user_id
+        self.number_card = number_card
 
+
+class UserModel(db.Model):
+    __tablename__ = 'user'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    password = db.Column(db.String, nullable=False)
+    email = db.Column(db.String, nullable=False, unique=True)
+    cards = db.relationship('CardModel', backref='user', lazy=True)
+
+    def __init__(self, name, password, email):
+        self.name = name
+        self.password = password
+        self.email = email
 
 with app.app_context():
     db.create_all()
@@ -91,7 +108,7 @@ def put():
     except ValueError as e:
         return {'message': str(e)}
 
-from  utils.generateNumberCards import randomNumberNotInserted 
+from  utils.generateNumberCards import randomNumberNotInsertedBingo 
 @app.route('/bingo/insere-numero-aleatorio', methods=['PUT'])
 def put2():
     try:
@@ -102,7 +119,7 @@ def put2():
 
         numbers = bingo.drawn_numbers.split(",") if bingo.drawn_numbers else list()
 
-        drawn_number = randomNumberNotInserted(numbers)
+        drawn_number = randomNumberNotInsertedBingo(numbers)
 
         if drawn_number is None:
             raise ValueError('Cartela Completa !')
@@ -118,7 +135,78 @@ def put2():
         return {'message': bingo.drawn_numbers}
     except ValueError as e:
         return {'message': str(e)}
+    
 
+from utils.generateNumberCards import generateNumberCards, generateUniqueRandomNumbersCards, formatCardsResponse
+@app.route('/cartela/cria-nova', methods=['POST'])
+# Cria cartela com sequencia única de numeros aléatorio
+def post2():
+    try:
+        req_body = request.json
+
+        user_id = req_body['userId']
+
+        if not user_id:
+            raise ValueError('User id é obrigatório !')
+        
+        user = UserModel.query.get(user_id)
+
+        if( not user):
+            raise ValueError('Usuario não encontrado !')
+        
+        bingo = BingoModel.query.order_by(BingoModel.created_at.desc()).first()
+        
+        if not bingo:
+            raise ValueError('Nenhum bingo encontrado !')
+        
+        user_has_card = CardModel.query.filter_by(bingo=bingo, user_id=user_id).first()
+
+        if(user_has_card):
+            raise ValueError('Cartela  já criada para o usuario!')
+        
+        # Pega todos os valores sorteados das catelas da atual rodada, e gera um cartela de valores únicos
+        all_drawn_numbers_sorted = [set(drawn_numbers_sorted.drawn_numbers.split(',')) for drawn_numbers_sorted in bingo.cards]
+
+        random_numbers_cards = generateUniqueRandomNumbersCards(all_drawn_numbers_sorted)
+
+        updated_drawn_numbers = ",".join(random_numbers_cards)
+
+        # Pega o ultimo valor de cartelas criada e soma + 1 para a atual
+        number_card = len(bingo.cards) + 1
+
+        card = CardModel(updated_drawn_numbers, bingo.id, user_id, number_card)
+
+        db.session.add(card)
+        db.session.commit()
+        return  jsonify( {'message': 'Cartela criada', 'cards':{'id': card.id, 'number_card': number_card, 'numbers': formatCardsResponse(card.drawn_numbers) }}), 200
+    except Exception as e:
+        return jsonify({'message': str(e)}), 400
+
+#https://medium.com/@hedgarbezerra35/api-rest-com-flask-autenticacao-25d99b8679b6
+from sqlalchemy.exc import IntegrityError
+@app.route('/usuario/criar', methods=['POST'])
+def post3():
+    try:
+        req_body = request.json      
+
+        name = req_body['name']
+        password = req_body['password']
+        email =  req_body['email']
+
+        if(not (name and password and email)):
+            raise ValueError('Nome, senha e email obrigatórios !')
+        
+        user = UserModel(name, password, email)
+        db.session.add(user)
+        db.session.commit()
+
+        return jsonify({'message': 'Usuario criado !'}), 200
+    except ValueError  as e:
+        return {'message': str(e)}
+    except IntegrityError as e:
+        db.session.rollback()
+        return jsonify({'message':'Usuario já existe !'}), 400
+    
 ## End Controller ##
 if __name__ == '__main__':
     app.run(debug=True)
--- api/utils/generateNumberCards.py
+++ api/utils/generateNumberCards.py
@@ -1,31 +1,80 @@
 import random 
 
 maxNumbersBingo = 75
-
-numberCards = 25
-    
-def generateNumberCards():
-    random_numbers_cards = set()
-
-
-    while (len(random_numbers_cards) < numberCards):
-        randomNumber = randomNumber()
-        random_numbers_cards.add(randomNumber)
-        
-    #isEquals = len(random_numbers_cards.difference(random_numbers_cards)) == 0
-    return random_numbers_cards
-   
+numberCards = 24
 
 def randomNumber():
     return random.randint(1, maxNumbersBingo)
 
 
-def randomNumberNotInserted(list_numbers):
+# Gera os numeros das carteals 
+def generateNumberCards():
+    random_numbers_cards = set()
+
+    while (len(random_numbers_cards) < numberCards):
+        _randomNumber = randomNumber()
+        random_numbers_cards.add(_randomNumber)
+        
+    return random_numbers_cards
+   
+
+# Função que gera numero aleatorio no bingo
+def randomNumberNotInsertedBingo(list_numbers):
     if len(list_numbers) >= maxNumbersBingo:
         return None
     
-    print(list_numbers)
     while(True):
         random_number = randomNumber()
         if not str(random_number) in list_numbers:
             return random_number
+
+
+# Verificar se 2 set são iguais 
+def isRandomNumbersEqualsCard(randomNumbers1, randomNumbers2):
+    return len(randomNumbers1.difference(randomNumbers2)) == 0
+
+
+# Gera cartela única
+# Parameters:
+# list_drawn_numbers_sorted (list): Lista com os valores das cartelas sorteados 
+
+def generateUniqueRandomNumbersCards(list_drawn_numbers_sorted):
+    numbers_cards = generateNumberCards()
+    numbers_cards = [str(item) for  item in numbers_cards ]
+
+    if (not list_drawn_numbers_sorted ):
+        return numbers_cards
+    
+    for drawns_numbers_sorted in list_drawn_numbers_sorted:
+        if(isRandomNumbersEqualsCard(drawns_numbers_sorted, numbers_cards)):
+            print('Igual')
+            return generateUniqueRandomNumbersCards(list_drawn_numbers_sorted)
+        
+    return numbers_cards
+
+
+def formatCardsResponse(drawn_numbers_sorted):
+    formatted_drawn_numbers = {
+        'b' : [],
+        'i' : [],
+        'n' : [],
+        'g' : [],
+        'o' : []
+    }
+
+    numbers = drawn_numbers_sorted.split(',')
+    for number in numbers:
+        number = int(number)
+        print(number)
+        if  number <=15 :
+            formatted_drawn_numbers['b'].append(number)
+        elif number <= 30:
+            formatted_drawn_numbers['i'].append(number)
+        elif number <= 45:
+            formatted_drawn_numbers['n'].append(number)
+        elif number <= 60:
+            formatted_drawn_numbers['g'].append(number)
+        elif number <= 75:
+            formatted_drawn_numbers['o'].append(number)
+    
+    return formatted_drawn_numbers
